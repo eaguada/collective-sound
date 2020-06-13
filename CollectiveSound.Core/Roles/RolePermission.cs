fcdc9fa70bfa0e5604a3fb5617e504c9feb37584
Jose Aguada <jeaguada@gmail.com>
2020-06-13 06:11:48 -0300

    sc01us001

--- CollectiveSound.Core/Roles/RolePermission.cs
+++ CollectiveSound.Core/Roles/RolePermission.cs
@@ -1,6 +1,16 @@
-﻿namespace CollectiveSound.Core.Roles
+﻿using CollectiveSound.Core.Permissions;
+using System;
+
+namespace CollectiveSound.Core.Roles
 {
     public class RolePermission
     {
+        public Guid RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+
+        public Guid PermissionId { get; set; }
+
+        public virtual Permission Permission { get; set; }
     }
 }
--- CollectiveSound.Core/Users/UserRole.cs
+++ CollectiveSound.Core/Users/UserRole.cs
@@ -1,10 +1,6 @@
 ﻿using CollectiveSound.Core.Roles;
 using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollectiveSound.Core.Users
 {
